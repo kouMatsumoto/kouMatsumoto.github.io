103b148cf2c9fdd1c426bce1e83b057f48466718
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-10-04 14:16:47 +0900

    feat: initial commit

---
+++ gulp/tasks/default.ts
@@ -0,0 +1,11 @@
+import * as gulp from 'gulp';
+
+
+gulp.task('default', ['help']);
+
+gulp.task('help', () => {
+  const taskList = Object.keys(gulp['tasks'])
+    .sort();
+
+  console.log(`\nHere's a list of supported tasks:\n   `, taskList.join('\n    '));
+});
