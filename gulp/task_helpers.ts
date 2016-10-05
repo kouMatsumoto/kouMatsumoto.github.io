a8d9d4047fce529de8dcc6906d7fa8afad4028da
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-10-05 22:01:54 +0900

    chore(gulp): Add task_helpers.ts and its dependencies

---
+++ gulp/task_helpers.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+import * as gulp from 'gulp';
+import * as gulpSass from 'gulp-sass';
+import * as gulpSourcemaps from 'gulp-sourcemaps';
+import * as path from 'path';
+
+
+
+/** If the string passed in is a glob, returns it, otherwise append '**\/*' to it. */
+function _globify(maybeGlob: string, suffix = '**/*') {
+  if (maybeGlob.indexOf('*') != -1) {
+    return maybeGlob;
+  }
+  try {
+    const stat = fs.statSync(maybeGlob);
+    if (stat.isFile()) {
+      return maybeGlob;
+    }
+  } catch (e) {}
+  return path.join(maybeGlob, suffix);
+}
+
+
+/** Create a SASS Build Task. */
+export function sassBuildTask(dest: string, root: string, includePaths: string[]) {
+  const sassOptions = { includePaths };
+
+  return () => {
+    return gulp.src(_globify(root, '**/*.scss'))
+      .pipe(gulpSourcemaps.init())
+      .pipe(gulpSass(sassOptions).on('error', gulpSass.logError))
+      .pipe(gulpSourcemaps.write('.'))
+      .pipe(gulp.dest(dest));
+  };
+}
