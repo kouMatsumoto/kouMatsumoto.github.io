bad42d312652f90bb5258db1910dd257f1349e79
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-10-08 15:16:15 +0900

    chore(gulp): Add task to watch

--- gulp/tasks/development.ts
+++ gulp/tasks/development.ts
@@ -6,12 +6,16 @@ import * as gulp from 'gulp';
 import {join} from 'path';
 import {sassBuildTask} from '../task_helpers';
 import {PROJECT_ROOT, SOURCE_ROOT} from '../constants';
+
+// This import is lack a type
 const gulpPug = require('gulp-pug');
 
+const pugFilesToBuild = join(SOURCE_ROOT, 'pug/**/*.pug');
+const scssFilesToBuild = join(SOURCE_ROOT, 'scss/**/*.scss');
 
 
 gulp.task('build:pug', () => {
-  return gulp.src(join(SOURCE_ROOT, 'pug/**/*.pug'))
+  return gulp.src(pugFilesToBuild)
     .pipe(gulpPug({
       pretty: true
     }))
@@ -19,4 +23,10 @@ gulp.task('build:pug', () => {
 });
 
 
-gulp.task('build:scss', sassBuildTask('css', join(SOURCE_ROOT, 'scss')));
+gulp.task('build:scss', sassBuildTask('css', scssFilesToBuild));
+
+
+gulp.task('watch', () => {
+  gulp.watch(pugFilesToBuild, ['build:pug']);
+  gulp.watch(scssFilesToBuild, ['build:scss']);
+});
