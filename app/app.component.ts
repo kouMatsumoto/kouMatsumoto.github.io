3ba61ec2f3517929bdbc8d2daa53079bf088558d
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-10-17 22:29:48 +0900

    update(app): Remove unused tutorial files

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard">Dashboard</a>
-      <a routerLink="/heroes">Heroes</a>
-    </nav>
-  
-    <router-outlet></router-outlet>
+    <md-toolbar color="primary">
+      <span>Application Title</span>
+    </md-toolbar>
   `
 })
 export class AppComponent {
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,44 +1,19 @@
-import './rxjs-extensions';
-
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroService } from './hero.service';
-
-import { AppRoutingModule } from './app-routing.module';
-
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
-import { InMemoryDataService }  from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search.component';
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule,
     MaterialModule.forRoot()
   ],
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    HeroSearchComponent
+    AppComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [],
   bootstrap: [ AppComponent ],
 })
 export class AppModule { }
