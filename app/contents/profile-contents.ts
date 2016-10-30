a708e4933444300a29f4a49eb8527d285ccc69ae
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-10-31 01:14:07 +0900

    refactor(profile-contents): Replace md-list with my-list

--- app/contents/profile-contents.ts
+++ app/contents/profile-contents.ts
@@ -9,31 +9,61 @@ import {Component} from '@angular/core';
   ]
 })
 export class MyProfileContents {
-  os = [
+  itemsForList = [
     {
-      subject: 'Ubuntu',
-      text: `I use this to development fundamentally.
-        This is my <a href="https://github.com/kouMatsumoto/ubuntu-setup-guide">set-up guide</a>.
-      `
+      head: 'OS',
+      contents: [
+        {
+          subject: 'Ubuntu',
+          text: `I use this to development fundamentally.
+            This is my <a href="https://github.com/kouMatsumoto/ubuntu-setup-guide" target="_blank">set-up guide</a>.
+          `
+        },
+        {
+          subject: 'Mac',
+          text: `I use this to check behavior of applications in MacOS environment.
+            This is my <a href="https://github.com/kouMatsumoto/macos-setup-guide" target="_blank">set-up guide</a>.
+          `
+        },
+        {
+          subject: 'Windows',
+          text: `I use this to check behavior of applications in Windows environment.
+            This is my <a href="https://github.com/kouMatsumoto/windows-setup-guide" target="_blank">set-up guide</a>.
+          `
+        },
+      ]
     },
     {
-      subject: 'Mac',
-      text: `I use this to check behavior of applications in MacOS environment.
-        This is my <a href="https://github.com/kouMatsumoto/macos-setup-guide">set-up guide</a>.
-      `
+      head: 'Editor and IDE',
+      contents: [
+        {
+          subject: 'WebStorm',
+          text: `Fundamentally IDE of web development.
+            I use this in Node.js and TypeScript project.
+          `
+        },
+        {
+          subject: 'Visual Studio Code',
+          text: `With small codes, sometimes I use.`
+        }
+      ]
     },
     {
-      subject: 'Windows',
-      text: `I use this to check behavior of applications in Windows environment.
-        This is my <a href="https://github.com/kouMatsumoto/windows-setup-guide">set-up guide</a>.
-      `
-    },
-  ];
-
-  ide = [
-    {
-      subject: 'WebStorm',
-      text: 'I use this in web development fundamentally.'
+      head: 'Devices',
+      contents: [
+        {
+          subject: 'Ergodox EZ',
+          text: `The ergonomic keyboard which we can flexibly customize whatever, keymap, keyswitches, keycaps, etc...
+            See <a href="https://ergodox-ez.com/" target="_blank">https://ergodox-ez.com/</a>
+          `
+        },
+        {
+          subject: 'Logicool G700s',
+          text: `This mouse can store customized keymap into on-board memory. (we can use on Ubuntu.)
+            See <a href="http://gaming.logitech.com/en-us/product/g700s-rechargeable-wireless-gaming-mouse" target="_blank">http://gaming.logitech.com/en-us/product/g700s-rechargeable-wireless-gaming-mouse</a>
+          `
+        }
+      ]
     }
   ];
 }
