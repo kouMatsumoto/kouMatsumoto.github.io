eda57f47bdadaa085e730fb511a9f74e941aa40a
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-11-08 23:02:40 +0900

    update: Change zone.js script tag loading to import

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+// Angular require zone.js polyfill.
+import 'zone.js/dist/zone.js';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app.module';
+
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
