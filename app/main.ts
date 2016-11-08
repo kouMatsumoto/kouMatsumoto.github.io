04dd9c69f6a38ec557b2708170012fc791c9cd17
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-11-08 23:11:57 +0900

    update: Change Reflect script tag loading to import

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
-// Angular require zone.js polyfill.
-import 'zone.js/dist/zone.js';
+// Angular require zone.js and Reflect polyfill.
+require('zone.js/dist/zone.js');
+require('reflect-metadata/Reflect.js');
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
