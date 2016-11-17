eddb4ed13d176ed90c77b7fc4c1e0cd28487beb1
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-11-17 22:58:36 +0900

    chore(gulp): update bundle task

--- gulp/constants.ts
+++ gulp/constants.ts
@@ -4,3 +4,4 @@ export const PROJECT_ROOT = join(__dirname, '..');
 export const SOURCE_ROOT = join(PROJECT_ROOT, 'src');
 export const APP_ROOT = join(PROJECT_ROOT, 'app');
 export const DIST_ROOT = join(PROJECT_ROOT, 'dist');
+export const TMP_ROOT = join(PROJECT_ROOT, 'tmp');
--- gulp/tasks/build.ts
+++ gulp/tasks/build.ts
@@ -6,10 +6,61 @@ import * as gulp from 'gulp';
 import * as gulpConcat from 'gulp-concat';
 import * as gulpUglify from 'gulp-uglify';
 import {join} from 'path';
-import {PROJECT_ROOT, DIST_ROOT} from '../constants';
+import {PROJECT_ROOT, DIST_ROOT, APP_ROOT, TMP_ROOT} from '../constants';
+import {copyTask, sassBuildTask, tsBuildTask} from '../task_helpers';
 
 // lack of types
 const systemjsBuilder = require('systemjs-builder');
+const inlineNg2Template= require('gulp-inline-ng2-template');
+
+
+/**
+ * Concatenate dependency files of ES6-polyfill, zone.js, reflect, and system.js,
+ */
+gulp.task('bundle:dependencies', () => {
+  const esDependencies = [
+    join(PROJECT_ROOT, 'node_modules/core-js/client/shim.min.js'),
+    join(PROJECT_ROOT, 'node_modules/zone.js/dist/zone.js'),
+    join(PROJECT_ROOT, 'node_modules/reflect-metadata/Reflect.js'),
+    join(PROJECT_ROOT, 'node_modules/systemjs/dist/system.src.js'),
+    join(PROJECT_ROOT, 'systemjs.config.js'),
+  ];
+
+  return gulp.src(esDependencies)
+    .pipe(gulpConcat('dependency.js'))
+    .pipe(gulpUglify())
+    .pipe(gulp.dest(DIST_ROOT));
+});
+
+
+/**
+ * A task to prepare bundle app
+ * To alter file contents, copy app files.
+ */
+gulp.task(':pre:bundle:copy', copyTask(APP_ROOT, TMP_ROOT));
+
+/**
+ * Compile scss to css to inject as ng2 template
+ */
+gulp.task(':pre:bundle:scss', sassBuildTask(TMP_ROOT, TMP_ROOT));
+
+/**
+ * Inject html and css templates to ts as string
+ */
+gulp.task(':pre:bundle:template', () => {
+  gulp.src(join(TMP_ROOT, '**/*.ts'))
+    .pipe(inlineNg2Template({
+      base: 'tmp',
+      useRelativePaths: true
+    }))
+    .pipe(gulp.dest(TMP_ROOT));
+});
+
+/**
+ * Compile ts files injected html and css templates
+ */
+gulp.task(':pre:bundle:ts', tsBuildTask(TMP_ROOT));
+
 
 /**
  * bundle for system.js
@@ -32,23 +83,4 @@ gulp.task('bundle:app', () => {
     .catch((err) => {
       console.error('Build Failed', err);
     });
-});
-
-
-/**
- * Concatenate dependency files of ES6-polyfill, zone.js, reflect, and system.js,
- */
-gulp.task('bundle:dependencies', () => {
-  const esDependencies = [
-    join(PROJECT_ROOT, 'node_modules/core-js/client/shim.min.js'),
-    join(PROJECT_ROOT, 'node_modules/zone.js/dist/zone.js'),
-    join(PROJECT_ROOT, 'node_modules/reflect-metadata/Reflect.js'),
-    join(PROJECT_ROOT, 'node_modules/systemjs/dist/system.src.js'),
-    join(PROJECT_ROOT, 'systemjs.config.js'),
-  ];
-
-  return gulp.src(esDependencies)
-    .pipe(gulpConcat('dependency.js'))
-    .pipe(gulpUglify())
-    .pipe(gulp.dest(DIST_ROOT));
 });
