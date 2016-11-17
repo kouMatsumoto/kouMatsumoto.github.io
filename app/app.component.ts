2ec455dfe24cbe082141549304dc48f148721fd4
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-11-17 23:18:17 +0900

    update(app): Change app.scss to use as global css

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   moduleId: module.id,
+  encapsulation: ViewEncapsulation.None,
   selector: 'my-app',
   templateUrl: './app.template.html',
   styleUrls: ['./app.css']
