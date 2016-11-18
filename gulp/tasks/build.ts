716f5e507306bb30651602fe0f03afe7f7865e5b
Kou Matsumoto <kou.matsumoto.jp@gmail.com>
2016-11-18 17:55:35 +0900

    chore(gulp): Update tasks for bundle:app with dependency task

--- gulp/tasks/build.ts
+++ gulp/tasks/build.ts
@@ -12,7 +12,6 @@ import {copyTask, sassBuildTask, tsBuildTask, cleanTask} from '../task_helpers';
 // lack of types
 const systemjsBuilder = require('systemjs-builder');
 const inlineNg2Template= require('gulp-inline-ng2-template');
-const runSequence= require('run-sequence');
 
 
 /**
@@ -42,19 +41,19 @@ gulp.task(':pre:bundle:clean', cleanTask([DIST_ROOT, TMP_ROOT]));
  * A task to prepare bundle app
  * To alter file contents, copy app files.
  */
-gulp.task(':pre:bundle:copy', copyTask(APP_ROOT, TMP_ROOT));
+gulp.task(':pre:bundle:copy', [':pre:bundle:clean'], copyTask(APP_ROOT, TMP_ROOT));
 
 /**
  * Compile scss to css to inject as ng2 template
  */
-gulp.task(':pre:bundle:scss', sassBuildTask(TMP_ROOT, TMP_ROOT));
+gulp.task(':pre:bundle:scss', [':pre:bundle:copy'], sassBuildTask(TMP_ROOT, TMP_ROOT));
 
 /**
  * Inject html and css templates to ts as string
  *
  * TODO: resolve error of .pipe (Unresolved function or method)
  */
-gulp.task(':pre:bundle:template', () => {
+gulp.task(':pre:bundle:template', [':pre:bundle:scss'], () => {
   return gulp.src(join(TMP_ROOT, '**/*.ts'))
     .pipe<any>(inlineNg2Template({
       base: 'tmp',
@@ -66,13 +65,13 @@ gulp.task(':pre:bundle:template', () => {
 /**
  * Compile ts files injected html and css templates
  */
-gulp.task(':pre:bundle:ts', tsBuildTask(TMP_ROOT));
+gulp.task(':pre:bundle:ts', [':pre:bundle:template'], tsBuildTask(TMP_ROOT));
 
 
 /**
  * bundle for system.js
  */
-gulp.task(':pre:bundle:app', () => {
+gulp.task('bundle:app', [':pre:bundle:ts'], () => {
   const builder = new systemjsBuilder(PROJECT_ROOT, 'systemjs.config.js');
   const src = join(DIST_ROOT, 'main.js');
   const dest = join(DIST_ROOT, 'app.js');
@@ -82,8 +81,7 @@ gulp.task(':pre:bundle:app', () => {
     sourceMaps: false
   };
 
-  builder
-    .bundle(src, dest, config)
+  builder.bundle(src, dest, config)
     .then(() => {
       console.log('Build Complete');
     })
@@ -91,14 +89,3 @@ gulp.task(':pre:bundle:app', () => {
       console.error('Build Failed', err);
     });
 });
-
-/**
- * A task to bundle app
- */
-gulp.task('bundle:app', () => runSequence(
-  ':pre:bundle:copy',
-  ':pre:bundle:scss',
-  ':pre:bundle:template',
-  ':pre:bundle:ts',
-  ':pre:bundle:app'
-));
