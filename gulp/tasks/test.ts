a5c0c9b3561b881f17f92329d82f58ddc86b2c6b
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-11-27 18:24:06 +0900

    chore(gulp/test): Update comments of task

--- gulp/tasks/test.ts
+++ gulp/tasks/test.ts
@@ -10,7 +10,7 @@ import {PROJECT_ROOT} from '../constants';
 
 /**
  * This task is assuming that TS files are already transpiled. (don't run this before ts task.)
- * This task is dependency of TODO
+ * This task is dependency of dev:app
  */
 gulp.task(':test:unit', (done) => {
   new Server({configFile: join(PROJECT_ROOT, 'karma.conf.js')}, done).start();
