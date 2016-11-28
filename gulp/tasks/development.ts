67d6bbc9f6bdb33660d98a9963c1788b512d598d
Kou Matsumoto <kou.matsumoto.jp@gmail.com>
2016-11-28 19:53:45 +0900

    chore(gulp/development): Add gulp task "styl:app:dev"

--- gulp/tasks/development.ts
+++ gulp/tasks/development.ts
@@ -4,11 +4,13 @@
 
 import * as gulp from 'gulp';
 import * as gulpRename from 'gulp-rename';
+import * as gulpSourcemaps from 'gulp-sourcemaps';
 import {join} from 'path';
 import {serverTask, tsBuildTask, copyTask, stylusBuildTask} from '../task_helpers';
 import {DIST_ROOT, APP_ROOT, PROJECT_ROOT} from '../constants';
 
 // lack of types
+const gulpStylus = require('gulp-stylus');
 const runSequence = require('run-sequence');
 
 
@@ -49,4 +51,16 @@ gulp.task('dev:app', ['build:app'], () => {
     ':test:unit',
     'watch:app'
   ]);
+});
+
+
+/**
+ * Task to transpile stylus for development
+ */
+gulp.task('styl:app:dev', () => {
+  return gulp.src([join(APP_ROOT, '**/*.styl')])
+    .pipe(gulpSourcemaps.init())
+    .pipe(gulpStylus())
+    .pipe(gulpSourcemaps.write('.'))
+    .pipe(gulp.dest(APP_ROOT));
 });
